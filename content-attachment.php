07647643c632b229034bed83289e2e3a0f4dcdfd
Taras Dashkevych <trdashix@gmail.com>
2016-09-03 09:38:28 -0700

    Updated attachment content template;

--- content-attachment.php
+++ content-attachment.php
@@ -1,8 +1,12 @@
 <?php
 /**
- * The default template for displaying attachment content
+ * The default template for displaying attachment content.
+ *
  * @package tdMacro
  */
+
+$metadata = wp_get_attachment_metadata();
+
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
@@ -11,26 +15,17 @@
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 
 		<div class="single-posted-on">
-		<?php
-				$metadata = wp_get_attachment_metadata();
-				printf( __( 'Published <span class="entry-date"><time class="entry-date" datetime="%1$s" pubdate>%2$s</time></span> at <a href="%3$s" title="Link to full-size image">%4$s &times; %5$s</a> in <a href="%6$s" title="Return to %7$s" rel="gallery">%7$s</a>', 'tdmacro' ),
-					esc_attr( get_the_date( 'c' ) ),
-					esc_html( get_the_date() ),
-					wp_get_attachment_url(),
-					$metadata['width'],
-					$metadata['height'],
-					get_permalink( $post->post_parent ),
-					get_the_title( $post->post_parent )
-				);
-			?>
-		<?php edit_post_link( __( 'Edit', 'tdmacro' ), ' &bull; <span class="edit-link">', '</span>' ); ?>
+            <span class="parent-post-link"><a href="<?php echo esc_url( get_permalink( $post->post_parent ) ); ?>" rel="gallery"><?php echo get_the_title( $post->post_parent ); ?></a></span>
+            <span class="entry-date"><time class="entry-date" datetime="<?php echo esc_attr( get_the_date( 'c' ) ); ?>"><?php echo esc_html( get_the_date() ); ?></time></span>
+            <span class="full-size-link"><a href="<?php echo esc_url( wp_get_attachment_url() ); ?>"><?php echo esc_html( $metadata['width'] ); ?> &times; <?php echo esc_html( $metadata['height'] ); ?></a></span>
+            <?php edit_post_link( esc_html__( 'Edit', 'tdmacro' ), '<span class="edit-link">', '</span>' ); ?>
 		</div><!-- .single-posted-on -->
 
 	</header><!-- .entry-header -->
 
-	<div class="entry-content">
-		<div class="entry-attachment">
-		<?php echo wp_get_attachment_image( $post->ID, 'full' );  ?>
-		</div><!-- .entry-attachment -->
-	</div><!-- .entry-content -->
+    <div class="entry-content">
+        <div class="entry-attachment">
+            <?php echo wp_get_attachment_image( get_the_ID(), 'full' );  ?>
+        </div><!-- .entry-attachment -->
+    </div><!-- .entry-content -->
 </article><!-- #post-## -->
