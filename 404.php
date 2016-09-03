3556a22e4aebe00f2dd6f6cf1e7bcad4784c9050
Taras Dashkevych <trdashix@gmail.com>
2016-09-03 08:40:32 -0700

    Removed unnecessary divs, escaped strings

--- 404.php
+++ 404.php
@@ -7,26 +7,21 @@
 
 get_header(); ?>
 
-<div class="container">
-	<div class="row">
-		<div id="primary" class="content-area">
-			<main id="main" class="site-main" role="main">
-				<section class="error-404 not-found col-lg-12">
-					<header class="page-header">
-						<h4 class="meta-404">404</h4>
-						<h1 class="page-title"><?php _e( 'Oops! That page can&rsquo;t be found.', 'tdmacro' ); ?></h1>
-					</header><!-- .page-header -->
+<div id="primary" class="content-area container">
+    <main id="main" class="site-main" role="main">
+        <section class="error-404 not-found">
+            <header class="page-header">
+                <h4 class="meta-404"><?php esc_html_e( '404', 'tdmacro' ); ?></h4>
+                <h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'tdmacro' ); ?></h1>
+            </header><!-- .page-header -->
 
-					<div class="page-content">
-						<p><?php _e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'tdmacro' ); ?></p>
+            <div class="page-content">
+                <p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try a search?', 'tdmacro' ); ?></p>
 
-						<?php get_search_form(); ?>
-
-					</div><!-- .page-content -->
-				</section><!-- .error-404 -->
-			</main><!-- #main -->
-		</div><!-- #primary -->
-	</div><!-- .row -->
-</div><!-- .container -->
+                <?php get_search_form(); ?>
+            </div><!-- .page-content -->
+        </section><!-- .error-404 -->
+    </main><!-- #main -->
+</div><!-- #primary -->
 
 <?php get_footer(); ?>
