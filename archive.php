36645768a2075643efbc1a90c01aeeeae9d31da3
Taras Dashkevych <trdashix@gmail.com>
2016-09-03 08:56:59 -0700

    Added core functions to archive template;

--- archive.php
+++ archive.php
@@ -11,83 +11,29 @@ get_header(); ?>
 
 <div class="container">
 	<div class="row">
-		<section id="primary" class="content-area <?php echo esc_attr( tdmacro_get_blog_primary_class() ); ?>">
+		<section id="primary" class="content-area col-lg-12">
 			<main id="main" class="site-main" role="main">
 
 			<?php if ( have_posts() ) : ?>
 
 				<header class="page-header">
-					<h2 class="page-title">
-						<?php
-							if ( is_category() ) :
-								single_cat_title();
-
-							elseif ( is_tag() ) :
-								single_tag_title();
-
-							elseif ( is_author() ) :
-								printf( __( 'Author: %s', 'tdmacro' ), '<span class="vcard">' . get_the_author() . '</span>' );
-
-							elseif ( is_day() ) :
-								printf( __( 'Day: %s', 'tdmacro' ), '<span>' . get_the_date() . '</span>' );
-
-							elseif ( is_month() ) :
-								printf( __( 'Month: %s', 'tdmacro' ), '<span>' . get_the_date( _x( 'F Y', 'monthly archives date format', 'tdmacro' ) ) . '</span>' );
-
-							elseif ( is_year() ) :
-								printf( __( 'Year: %s', 'tdmacro' ), '<span>' . get_the_date( _x( 'Y', 'yearly archives date format', 'tdmacro' ) ) . '</span>' );
-
-							elseif ( is_tax( 'post_format', 'post-format-aside' ) ) :
-								_e( 'Asides', 'tdmacro' );
-
-							elseif ( is_tax( 'post_format', 'post-format-gallery' ) ) :
-								_e( 'Galleries', 'tdmacro');
-
-							elseif ( is_tax( 'post_format', 'post-format-image' ) ) :
-								_e( 'Images', 'tdmacro');
-
-							elseif ( is_tax( 'post_format', 'post-format-video' ) ) :
-								_e( 'Videos', 'tdmacro' );
-
-							elseif ( is_tax( 'post_format', 'post-format-quote' ) ) :
-								_e( 'Quotes', 'tdmacro' );
-
-							elseif ( is_tax( 'post_format', 'post-format-link' ) ) :
-								_e( 'Links', 'tdmacro' );
-
-							elseif ( is_tax( 'post_format', 'post-format-status' ) ) :
-								_e( 'Statuses', 'tdmacro' );
-
-							elseif ( is_tax( 'post_format', 'post-format-audio' ) ) :
-								_e( 'Audios', 'tdmacro' );
-
-							elseif ( is_tax( 'post_format', 'post-format-chat' ) ) :
-								_e( 'Chats', 'tdmacro' );
-
-							else :
-								_e( 'Archives', 'tdmacro' );
-
-							endif;
-						?>
-					</h2>
-					<?php
-						// Show an optional term description.
-						$term_description = term_description();
-						if ( ! empty( $term_description ) ) :
-							printf( '<div class="taxonomy-description">%s</div>', $term_description );
-						endif;
-					?>
+                <?php
+					the_archive_title( '<h1 class="page-title">', '</h1>' );
+					the_archive_description( '<div class="taxonomy-description">', '</div>' );
+				?>
 				</header><!-- .page-header -->
 
 				<div id="blog-grid" class="row content-grid">
 				<?php while ( have_posts() ) : the_post(); ?>
-					<div class="<?php echo esc_attr( tdmacro_column_class() ); ?> post-box">
-					<?php get_template_part( 'content', get_post_format() ); ?>
-					</div><!-- .col -->
+                    
+					<div class="col-lg-4 col-md-4 three-columns post-box">
+					   <?php get_template_part( 'content', get_post_format() ); ?>
+					</div><!-- .post-box -->
+                    
 				<?php endwhile; ?>
 				</div><!-- .row -->
 
-				<?php tdmacro_paging_nav(); ?>
+				<?php the_posts_navigation(); ?>
 
 			<?php else : ?>
 
