570bd08d4261377fbcf5d21dcb125082f397b64f
Taras Dashkevych <trdashix@gmail.com>
2016-09-03 16:46:59 -0700

    Removed unnecessary functions;

--- inc/extras.php
+++ inc/extras.php
@@ -108,80 +108,15 @@ function tdmacro_setup_author() {
 add_action( 'wp', 'tdmacro_setup_author' );
 
 /**
- * Blog Layout
- *
- * @since tdmacro 1.0
- */
-function tdmacro_column_class() {
-	return 'col-lg-4 col-md-4 three-columns';
-}
-
-/**
- * Return a column class for primary blog container
- *
- * @since tdmacro 1.0
- */
-function tdmacro_get_blog_primary_class() {
-
-	if( is_singular() ) {
-		return 'col-lg-8 col-md-8';
-	} else {
-		return 'col-lg-12';
-	}
-}
-
-/**
- * Custom render fucntion for the Jetpack Infinite Scroll
- *
- * @since tdmacro 1.0
- */
-function tdmacro_infinite_scroll_render() {
-	echo '<div class="infinite-wrap">';
-	while( have_posts() ) {
-		the_post();
-		echo '<div class="'.esc_attr( tdmacro_column_class() ).' post-box infinite-scroll-item">';
-		get_template_part( 'content', get_post_format() );
-		echo '</div><!-- .col -->';
-	}
-	echo '</div><!-- .infinite-wrap -->';
-}
-
-/**
- * Author Section / Single Post Pages
- *
- * @since tdmacro 1.0
- */
-function tdmacro_author_section() {
-	global $post;
-
-	$author_archive_link = get_author_posts_url( $post->post_author );
-
-	$author_section = '<div class="author-section">';
-	$author_section .= '<div class="about clearfix"><div class="gravatar">'. get_avatar( $post->post_author, 192 ) . '</div><!-- .gravatar -->';
-
-	$author_section .= '<div class="info">';
-	$author_section .= '<h4 class="author-section-header"><span>'.__( 'About', 'tdmacro' ).'</span><a class="author-name" href="'.esc_url( $author_archive_link ).'">'.get_the_author().'</a></h4>';
-	$author_section .= '<div class="about-description">'.nl2br( get_the_author_meta( 'description', $post->post_author ) ).'</div>';
-	$author_section .= '</div><!-- .info -->';
-	$author_section .= '</div> <!-- .about -->';
-
-	$author_section .= '</div><!-- .author-section -->';
-
-	return $author_section;
-}
-
-/**
  * Checks if user wants to hide/show post content when post has a featured image
  *
  * @since tdmacro 1.0
  */
 function tdmacro_show_featured_image_only() {
-	$selected_option = get_theme_mod( 'tdmacro_is_featured_image_without_content', 'hide' );
-
-	if( $selected_option === 'hide' ) {
-		return true;
-	} else {
+	if( 'hide' != get_theme_mod( 'tdmacro_is_featured_image_without_content', 'hide' ) ) {
 		return false;
+	} else {
+		return true;
 	}
 }
 
